be2a02d5297ed6b53382520633c27394e225907d
Brandon Tom <magicink@gmail.com>
2025-02-09 07:34:06 -0800

    initial commit

---
+++ src/lib/shared/EventBus/index.ts
@@ -0,0 +1,5 @@
+// Used to emit events between React components and Phaser scenes
+// https://newdocs.phaser.io/docs/3.70.0/Phaser.Events.EventEmitter
+import { Events } from 'phaser'
+
+export const EventBus = new Events.EventEmitter()
